8e08c69e1d0881b199266587a423880bb00061c0
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-03-24 16:48:51 +0100

    Style Form

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,20 +5,23 @@ import { LuShoppingBag } from "react-icons/lu";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-25 flex justify-between items-center  ">
-      <div className="h-15 w-15 flex items-center justify-center bg-white  ">
-        <MdFastfood />
+    <div className="w-full h-25 flex justify-between items-center px-5 ">
+      <div className="h-15 w-15 rounded-md shadow-xl flex items-center justify-center bg-white  ">
+        <MdFastfood className="h-7.5 w-7.5 text-purple-500 " />
       </div>
-      <div>
-        <form action="">
+      
+        <form className="w-[60%] rounded-md shadow-xl h-15 flex items-center px-5 bg-white ">
           <IoSearch />
           <input type="text" placeholder="Search..." />
         </form>
+      
+
+      <div className="h-15 w-15 rounded-md flex shadow-xl items-center justify-center bg-white  ">
+        <LuShoppingBag className="h-7.5 w-7.5 text-purple-500 " />
       </div>
 
-      <div className="h-15 w-15 flex items-center justify-center bg-white  ">
-        <LuShoppingBag />
-      </div>
+          
+
     </div>
   );
 };
