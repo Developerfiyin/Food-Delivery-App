92c21a7d32f02ddd4eaab540af26bf544a9c60e9
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-03-30 21:42:17 +0100

    Update Card2.jsx

--- src/Components/Card2.jsx
+++ src/Components/Card2.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
-
+import image from "../assets/image1.avif"
 const Card2 = () => {
   return (
-    <div className='w-full h-30 bg-blue-700 '> 
-        <div className='w-[60%] h-full bg-red-500 '>
-    <div className='w-[50%] h-full overflow-hidden '>
-        <img src="" alt="" />
+    <div className='w-full h-30 bg-blue-700 p-2 '> 
+        <div className='w-[60%] h-full bg-red-500 flex gap-5 '>
+    <div className='w-[60%] h-full overflow-hidden rounded-lg '>
+        <img src={image} alt=" The Image" className='object-cover' />
     </div>
-    <div>
+    <div className=' w-[40%] flex flex-col h-full  gap-5 '>
 
-        <div> Pancake</div>
+        <div className='text-lg text-blue-500 font-semibold '> Pancake</div>
+
+        <div>
+            <button>-</button>
+            <span>1</span>
+            <button>+</button>
+        </div>
         <div></div>
     </div>
 
