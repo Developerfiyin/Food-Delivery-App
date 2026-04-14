e6ebf44db11882a3505ca64028352ffde20da8a1
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-04-14 14:49:27 +0100

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { MdFastfood } from "react-icons/md";
+
 
 const Navbar = () => {
   return (
-    <div className ="bg-red-600 w-full h-[100px] bg-slate-400 " > 
+    <div className =" w-full h-25 bg-slate-400 flex justify-between  " > 
+
+    <div className='h-15 w-15 bg-white flex justify-center items-center '>
+<MdFastfood />
+    </div>
+    <form action="">
+<input type="text" />
+
+
+    </form>
 
  hiwiejiwigjgj
     </div>
