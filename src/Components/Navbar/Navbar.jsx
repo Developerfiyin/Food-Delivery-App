4a1b86bfad8fcc02da17457a8e1ed53f9bf78c56
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-04-14 15:51:06 +0100

    create a form for input and icon

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React from "react";
 import { MdFastfood } from "react-icons/md";
+import { IoSearch } from "react-icons/io5";
+import { LuShoppingBag } from "react-icons/lu";
 
 
 const Navbar = () => {
   return (
-    <div className =" w-full h-25 bg-slate-400 flex justify-between  " > 
+    <div className=" w-full h-25 bg-slate-400 flex justify-between  ">
+      <div className="h-15 w-15 bg-white flex justify-center items-center ">
+        <MdFastfood />
+      </div>
+      <form action="">
+        <IoSearch />
 
-    <div className='h-15 w-15 bg-white flex justify-center items-center '>
-<MdFastfood />
+        <input type="text" placeholder="Search Items...." />
+      </form>
+
+
+      <LuShoppingBag />
+      hiwiejiwigjgj
     </div>
-    <form action="">
-<input type="text" />
+  );
+};
 
-
-    </form>
-
- hiwiejiwigjgj
-    </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
