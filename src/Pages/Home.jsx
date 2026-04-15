84020cf16a97656a11f867e60b3eaafde35ba390
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-04-15 14:10:27 +0100

    Mapped over the items in card

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,22 +6,22 @@ const Home = () => {
   return (
     <div className="w-full min-h-screen bg-slate-200 ">
       <Navbar />
-        
-        <div className="flex flex-wrap justify-center items-center gap-5 w-full ">
 
-{Categories.map((category) => (
-  <div key={category.id} className="flex flex-col bg-white gap-5 w-35.5 h-37.5 items-start p-5 justify-start
+      <div className="flex flex-wrap justify-center items-center gap-5 w-full ">
+        {Categories.map((category) => (
+          <div
+            key={category.id}
+            className="flex flex-col bg-white gap-5 w-35.5 h-37.5 items-start p-5 justify-start
    shadow-xl rounded-lg  text-purple-500 text-[20px] hover:bg-purple-300 cursor-pointer transition-all duration-300
   
   
-  ">
-    {category.icon}
-    <span>{category.name}</span>
-  </div>
-))}
-
-        </div>
-     
+  "
+          >
+            {category.icon}
+            <span>{category.name}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
