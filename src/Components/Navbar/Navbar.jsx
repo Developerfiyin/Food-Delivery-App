ca86c8fec727f4bdc63054b933eb3d8e70f55ae4
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-04-15 12:37:09 +0100

    Update  Home.jsx file

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,19 +5,19 @@ import { LuShoppingBag } from "react-icons/lu";
 
 const Navbar = () => {
   return (
-    <div className=" w-full h-25 bg-slate-400 flex justify-between px-6 items-center ">
+    <div className=" w-full h-25 bg-slate-200 flex justify-between px-6 items-center md:px-8 ">
       <div className="h-15 w-15 bg-white flex justify-center items-center rounded-md ">
         <MdFastfood className="w-7.5 h-7.5 text-purple-400 shadow-xl" />
       </div>
 
-      <form className="flex items-center shadow-md h-15 w-[60%] gap-5 bg-white px-5 py-2 rounded-md"> 
+      <form className="flex  items-center shadow-md h-15 w-[45%] gap-5 bg-white px-5 py-2 rounded-md"> 
         <IoSearch className="text-purple-400 w-5 h-5 rounded-md "  />
-        <input type="text" placeholder="Search Items...." className="w-full outline-none text-[20px]  " />
+        <input type="text" placeholder="Search Items...." className="w-full outline-none text-[16px] md:text-[20px] " />
       </form>
 
 
       <div className=" h-15 w-15 bg-white flex relative justify-center items-center rounded-md">
-        <span className="absolute top-0 right-2 font-bold text-purple-400">0</span>
+        <span className="absolute top-0 right-2 font-bold text-purple-400 text-[18px]">0</span>
         <LuShoppingBag className="w-7.5 h-7.5 text-purple-500 shadow-xl" />
       </div>
       
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
-
+import Categories from "../Category";
 
 const Home = () => {
   return (
-    <div className="w-full min-h-screen bg-blue-500 ">
+    <div className="w-full min-h-screen bg-slate-200 ">
+      <Navbar />
+        
+        <div className="flex flex-wrap justify-center items-center gap-5 w-full ">
 
+{Categories.map((category) => (
+  <div key={category.id} className="flex flex-col bg-white gap-5 w-35.5 h-37.5 items-start p-5 justify-start
+   shadow-xl rounded-lg  text-purple-500 text-[20px] hover:bg-purple-300 cursor-pointer transition-all duration-300
+  
+  
+  ">
+    {category.icon}
+    <span>{category.name}</span>
+  </div>
+))}
 
-<Navbar/>
-
+        </div>
+     
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
