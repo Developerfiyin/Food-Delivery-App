d9c4af989804b2914518f28dae39f87a78d9a596
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-04-18 20:06:26 +0100

    Update card2

--- src/Components/Card2.jsx
+++ src/Components/Card2.jsx
@@ -7,18 +7,18 @@ const Card2 = () => {
     <div className="w-full h-30 bg-red-600 p-2 ">
       <div className=" w-[60%] h-full bg-purple-600 flex ">
         <div className="w-[60%] h-full bg-white overflow-hidden rounded-lg">
-          <img src={image} alt="Pancake" className=" object-cover" />
+          <img src={image} alt="Pancake" className=" object-cover"/>
         </div>
         <div className="flex flex-col gap-5 w-[40%] h-full ">
           <div className="text-lg font-semibold text-gray-600">Pancake</div>
-          <div>
+          <div className="bg-slate-400 ">
 
             <button>-</button>
             <span>0</span>
             <button>+</button>
           </div>
         </div>
-      </div>
+      </div>z
       <div></div>
     </div>
   );
