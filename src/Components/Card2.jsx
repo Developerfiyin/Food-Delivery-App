1fa5bc4556c3f28d85396df7732df1d9381c0122
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-04-18 12:20:46 +0100

    Create a card2.jsx file, add the image and button

--- src/Components/Card2.jsx
+++ src/Components/Card2.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 
+import image from "../assets/image1.avif";
+
 const Card2 = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <img src="" alt="" />
+    <div className="w-full h-30 bg-red-600 p-2 ">
+      <div className=" w-[60%] h-full bg-purple-600 flex ">
+        <div className="w-[60%] h-full bg-white overflow-hidden rounded-lg">
+          <img src={image} alt="Pancake" className=" object-cover" />
         </div>
-        <div>
-          <div>Pancake</div>
-          <div></div>
+        <div className="flex flex-col gap-5 w-[40%] h-full ">
+          <div className="text-lg font-semibold text-gray-600">Pancake</div>
+          <div>
+
+            <button>-</button>
+            <span>0</span>
+            <button>+</button>
+          </div>
         </div>
       </div>
       <div></div>
