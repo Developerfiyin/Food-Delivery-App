fc3e1b633032e6b7ec61de20f1d6f38d509be308
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-04-19 20:18:12 +0100

    Update card2

--- src/Components/Card2.jsx
+++ src/Components/Card2.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { RiDeleteBin5Line } from "react-icons/ri";
 
 import image from "../assets/image1.avif";
 
@@ -12,13 +13,24 @@ const Card2 = () => {
         <div className="flex flex-col gap-5 w-[40%] h-full  ">
           <div className="text-lg font-semibold text-slate-500">Pancake</div>
           <div className="bg-slate-400 h-12.5 w-27.5 flex rounded-lg  overflow-hidden shadow-lg font-semibold border-2 border-purple-400 text-xl ">
-            <button className="w-[30%] bg-white flex justify-center items-center text-purple-300 hover:bg-slate-100 ">-</button>
-            <span className="w-[40%] h-full flex justify-center items-center text-lg bg-slate-300 text-purple-400 ">0</span>
-            <button className="w-[30%] bg-white flex justify-center items-center text-purple-300 hover:bg-slate-100 ">+</button>
+            <button className="w-[30%] bg-white flex justify-center items-center text-purple-300 hover:bg-slate-100 ">
+              -
+            </button>
+            <span className="w-[40%] h-full flex justify-center items-center text-lg bg-slate-300 text-purple-400 ">
+              0
+            </span>
+            <button className="w-[30%] bg-white flex justify-center items-center text-purple-300 hover:bg-slate-100 ">
+              +
+            </button>
           </div>
         </div>
       </div>
-      <div></div>
+
+      <div className=" flex flex-col justify-center items-end gap-6  ">
+        <span className="text-xl text-purple-300 font-semibold "> $299</span>
+
+        <RiDeleteBin5Line className=" w-7.5 h-7.5  text-red-400 "  />
+      </div>
     </div>
   );
 };
