9c07ae3b44f9be024cfb6f6804533805eaf1bd72
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-04-20 17:39:26 +0100

    Import create slice from react toolkit

--- src/Components/redux/cartSlice.js
+++ src/Components/redux/cartSlice.js
@@ -1,2 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: [],
+    reducers: {
+        AddItems: (state, action) => {
+            state.push(action.payload);
+        },
+    }
+});
+
+export const { AddItems } = cartSlice.actions;
+export default cartSlice.reducer;
+                                                        
