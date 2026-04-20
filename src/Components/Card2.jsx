bb9b37d88e0eea124a651b1fd87adf5c1ed86d20
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-04-20 23:59:27 +0100

    Update Card 2

--- src/Components/Card2.jsx
+++ src/Components/Card2.jsx
@@ -3,21 +3,21 @@ import { RiDeleteBin5Line } from "react-icons/ri";
 
 import image from "../assets/image1.avif";
 
-const Card2 = () => {
+const Card2 = ({name, image, price, qty, id }) => {
   return (
-    <div className="w-full h-30 p-2 shadow-lg flex justify-center ">
+    <div className="w-faull h-30 p-2 shadow-lg flex justify-center ">
       <div className=" w-[60%] h-full  flex  gap-5">
         <div className="w-[60%] h-full bg-white overflow-hidden rounded-lg ">
           <img src={image} alt="Pancake" className=" object-cover " />
         </div>
         <div className="flex flex-col gap-5 w-[40%] h-full  ">
-          <div className="text-lg font-semibold text-slate-500">Pancake</div>
+          <div className="text-lg font-semibold text-slate-500"> {name}</div>
           <div className="bg-slate-400 h-12.5 w-27.5 flex rounded-lg  overflow-hidden shadow-lg font-semibold border-2 border-purple-400 text-xl ">
             <button className="w-[30%] bg-white flex justify-center items-center text-purple-300 hover:bg-slate-100">
               -
             </button>
             <span className="w-[40%] h-full flex justify-center items-center text-lg  bg-slate-300 text-purple-400 ">
-              0
+              {qty}
             </span>
             <button className="w-[30%] bg-white flex justify-center items-center text-purple-300 hover:bg-slate-100 ">+ </button>
           </div>
@@ -25,7 +25,7 @@ const Card2 = () => {
         
       </div>
        <div className=" flex flex-col justify-start items-end gap-6  ">
-        <span className="text-xl text-purple-300 font-semibold "> $299</span>
+        <span className="text-xl text-purple-300 font-semibold "> {price}</span>
         <RiDeleteBin5Line className=" w-7.5 h-7.5  text-red-400 "  />
       </div>
     </div>
